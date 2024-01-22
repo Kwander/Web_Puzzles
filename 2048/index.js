fb9e4ad581587ea4909e186f72af2fd526b2fe51
Kevin Wander <87961130+Kwander@users.noreply.github.com>
2024-01-22 17:41:54 -0500

    Html and CSS complete for 2048

---
+++ 2048/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const boxes = document.querySelectorAll('.box');
+    const reset_button = document.querySelector('#reset');
+    const span = document.querySelector(".actual_score");
+    // span.innerText = "new score";
+
+
+    
+    
+
+    function slideLeft(){
+
+    }
+
+
+});
